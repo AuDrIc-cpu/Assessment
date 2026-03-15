feb097cedadf346dfdf7ed54741bec2a3ec8366c
audric <alexanderwild734@gmail.com>
2026-03-16 12:07:17 +1300

    inputs

--- code_1.py
+++ code_1.py
@@ -6,11 +6,14 @@ print("2       mountain bike           hard                   900")
  
 
 
- #questions for the person who is taking the order thingy  
+#questions for the person who is taking the order thingy  
 input("What is your name? ") 
 int(input(f"What is your age? "))
 age = 0
 while age < 13 :
     input("What number camp do you want to go to? ")
+    input(f"What meal do you want?: standard, vegan or veganvegan ")
+    input(f"Do you need shuttle bus? extra cost is $80 ")
+    if 
 if age >= 12 :
   print("sorry you are not within the age limit to go.")
