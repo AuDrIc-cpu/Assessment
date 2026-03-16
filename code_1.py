3a2700ebf643736f0a1e9bfc6837e9bd860a213e
Audric <alexanderwild734@gmail.com>
2026-03-17 12:50:44 +1300

    took off heyhey

--- code_1.py
+++ code_1.py
@@ -5,7 +5,6 @@ print("num    activities              difficulty             cost")
 print("0    cultural immersion          easy                  800")
 print("1     Kayaking & pancakes       moderate               400")
 print("2       mountain bike           hard                   900")
-print("heyhey")
  
 
 
