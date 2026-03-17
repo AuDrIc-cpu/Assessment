575b01996a62db8e13b71f493ae4f401e82a70cb
Audric <alexanderwild734@gmail.com>
2026-03-17 14:40:10 +1300

    camp age

--- code_1.py
+++ code_1.py
@@ -4,6 +4,12 @@ food = ["standard","vegan","veganvegan"]
 activities = ["Cultural immersion","Kayaking & pancakes","Mountain bike"]
 difficulty = ["easy","moderate","hard"]
 cost = ["800","400","900"]
+age_min = 5 
+age_max = 17 
+age = 0
+camp_leader_age = 15
+Shuttle = ""
+camp_age = ""
 
 #print 
 print("num    activities              difficulty             cost")    
@@ -13,14 +19,22 @@ print("2       Mountain bike           hard                   900")
  
 #questions for the person who is taking the order thingy  
 input("What is your name? ") 
-int(input(f"What is your age? "))
-age = 0
-if age<13 :
+
+#To make sure that they put a number on the camp age 
+while camp_age == "" or camp_age.isdigit() == False :
+  camp_age = input("What is your age? ")
+  if camp_age == "" or camp_age.isdigit() == False: print("you need to enter your age.")
+
+#no more question if you dont reach the eligibility 
+while int(camp_age) > age_min and int(camp_age) < age_max : 
+if age >= camp_leader_age :
+  print("You are also eligible to be a camp leader!")
+if age < age_min :
     int(input("What number camp do you want to go to? "))
     input(f"What meal do you want?: standard, vegan or veganvegan ")
     input(f"Do you need shuttle bus? extra cost is $80 ")
-if  Shuttle != yes: 
+elif  Shuttle != "yes": 
   print("alright") 
   print("Alright so you picked number {num[1]} which is {activities[1]} and is {difficulty[1]}, your total is {cost[1]} ")
-elif age > 12 : 
-  print("sorry you are not eligible to come.")    
+    
+
